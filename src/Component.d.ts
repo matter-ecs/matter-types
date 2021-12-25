5f43f2b164dca5992b23860a99b6bd7153195bae
Marcus <Ukendio@gmail.com>
2021-12-25 13:10:58 +0100

    component patch

--- src/Component.d.ts
+++ src/Component.d.ts
@@ -2,13 +2,26 @@ export type AnyComponent = Component<unknown>;
 
 export type ComponentBundle = Array<AnyComponent>;
 
+type OptionalPropertyNames<T> = { [K in keyof T]: undefined extends T[K] ? K : never }[keyof T];
+
+type SpreadProperties<L, R, K extends keyof L & keyof R> = { [P in K]: L[P] | Exclude<R[P], undefined> };
+
+type Id<T> = { [K in keyof T]: T[K] };
+
+type Spread<L, R> = Id<
+	Pick<L, Exclude<keyof L, keyof R>> &
+		Pick<R, Exclude<keyof R, OptionalPropertyNames<R>>> &
+		Pick<R, Exclude<OptionalPropertyNames<R>, keyof L>> &
+		SpreadProperties<L, R, OptionalPropertyNames<R> & keyof L>
+>;
+
 export class Component<T> {
 	/*@internal */
 	public internal: T;
 
 	public constructor(data: T);
 
-	public patch(data: T): Component<T>;
+	public patch<U>(data: U): Component<Spread<T, U>>;
 }
 
 export function newComponent<T>(name?: string): {
