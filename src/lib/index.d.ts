a393345573542ff586bc046d7a6ae998604c7e88
Marcus <ukendio@gmail.com>
2022-01-30 17:43:24 +0100

    export it from index.d.ts

--- src/lib/index.d.ts
+++ src/lib/index.d.ts
@@ -3,7 +3,8 @@ import { useDeltaTime } from "./hooks/useDeltaTime";
 import { useEvent } from "./hooks/useEvent";
 import { useThrottle } from "./hooks/useThrottle";
 import { Loop } from "./Loop";
+import { useHookState } from "./TopoRuntime";
 import { AnyEntity, Entity, World } from "./World";
 
-export { World, newComponent as component, Loop, useEvent, useDeltaTime, useThrottle };
+export { World, newComponent as component, Loop, useEvent, useDeltaTime, useThrottle , useHookState};
 export type { Entity, AnyEntity, Component, AnyComponent };
