f7c2f250ab7aa4d380eefe18095989c7313f5be8
Marcus <ukendio@gmail.com>
2022-02-22 07:42:01 +0100

    export useful utility type

--- src/lib/index.d.ts
+++ src/lib/index.d.ts
@@ -1,4 +1,4 @@
-import { AnyComponent, Component, newComponent } from "./Component";
+import { AnyComponent, Component, newComponent, InferComponents } from "./Component";
 import { useDeltaTime } from "./hooks/useDeltaTime";
 import { useEvent } from "./hooks/useEvent";
 import { useThrottle } from "./hooks/useThrottle";
@@ -8,4 +8,4 @@ import { useHookState } from "./TopoRuntime";
 import { AnyEntity, Entity, World } from "./World";
 
 export { World, newComponent as component, Loop, useEvent, useDeltaTime, useThrottle , useHookState, None};
-export type { Entity, AnyEntity, Component, AnyComponent };
+export type { Entity, AnyEntity, Component, AnyComponent , InferComponents};
