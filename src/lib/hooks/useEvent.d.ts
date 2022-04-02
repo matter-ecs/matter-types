1efaadcb4b9b6b9e56ad7ff1638c0ab39ee78b7c
λ <46044567+Rimuy@users.noreply.github.com>
2022-04-02 14:20:16 -0300

    Change id to index
    You're not querying components, just iterating over an event.

--- src/lib/hooks/useEvent.d.ts
+++ src/lib/hooks/useEvent.d.ts
@@ -7,9 +7,9 @@ type InferSignalParameters<S> = S extends SignalLike ? Parameters<Parameters<S["
 export function useEvent<I extends Instance, E extends InstanceEventNames<I>>(
 	instance: I,
 	event: E,
-): IterableFunction<LuaTuple<[id: number, ...rest: InferSignalParameters<InstanceEvents<I>[E]>]>>;
+): IterableFunction<LuaTuple<[index: number, ...rest: InferSignalParameters<InstanceEvents<I>[E]>]>>;
 
 export function useEvent<E extends SignalLike>(
 	discriminator: string,
 	event: E,
-): IterableFunction<LuaTuple<[id: number, ...rest: InferSignalParameters<E>]>>;
+): IterableFunction<LuaTuple<[index: number, ...rest: InferSignalParameters<E>]>>;
