1881e34aba851698e6dac6eca1a3308cd8acf8dc
λ <46044567+Rimuy@users.noreply.github.com>
2022-04-02 12:11:46 -0300

    Update useEvent.d.ts

--- src/lib/hooks/useEvent.d.ts
+++ src/lib/hooks/useEvent.d.ts
@@ -1,13 +1,15 @@
-type Event<T extends Instance, P extends keyof T> = T[P] extends RBXScriptSignal ? T[P] : never;
+interface SignalLike {
+	Connect(callback: (...args: Array<any>) => void): void;
+}
 
-export function useEvent<I extends Instance, S extends keyof I, E extends Event<I, S>>(
-	inst: I,
-	event: S,
-): IterableFunction<LuaTuple<[number, ...Parameters<GenericOfRBXScriptSignal<E>>]>>;
+type InferSignalParameters<S> = S extends SignalLike ? Parameters<Parameters<S["Connect"]>[0]> : never;
 
-export function useEvent<I extends Instance, E extends RBXScriptSignal>(
-	inst: I,
+export function useEvent<I extends Instance, E extends InstanceEventNames<I>>(
+	instance: I,
 	event: E,
-): IterableFunction<LuaTuple<[number, ...Parameters<GenericOfRBXScriptSignal<E>>]>>;
+): IterableFunction<LuaTuple<[id: number, ...rest: InferSignalParameters<InstanceEvents<I>[E]>]>>;
 
-type GenericOfRBXScriptSignal<T> = T extends RBXScriptSignal<infer A> ? A : never;
+export function useEvent<E extends SignalLike>(
+	discriminator: string,
+	event: E,
+): IterableFunction<LuaTuple<[id: number, ...rest: InferSignalParameters<E>]>>;
