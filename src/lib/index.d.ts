d9abbd0ab0a75a16e38442f3e29530c9a67a7568
Marcus <ukendio@gmail.com>
2022-04-03 11:57:31 +0200

    export utility types

--- src/lib/index.d.ts
+++ src/lib/index.d.ts
@@ -1,11 +1,11 @@
-import { AnyComponent, Component, newComponent, InferComponents } from "./Component";
+import { AnyComponent, Component, newComponent, InferComponents, GenericOfComponent} from "./Component";
 import { useDeltaTime } from "./hooks/useDeltaTime";
 import { useEvent } from "./hooks/useEvent";
 import { useThrottle } from "./hooks/useThrottle";
 import { Loop } from "./Loop";
 import None from "./None";
 import { useHookState } from "./TopoRuntime";
-import { AnyEntity, Entity, World } from "./World";
+import { AnyEntity, Entity, World , Iterate } from "./World";
 
-export { World, newComponent as component, Loop, useEvent, useDeltaTime, useThrottle , useHookState, None};
-export type { Entity, AnyEntity, Component, AnyComponent , InferComponents};
+export { World, newComponent as component, Loop, useEvent, useDeltaTime, useThrottle , useHookState, None,};
+export type { Entity, AnyEntity, Component, AnyComponent , InferComponents, GenericOfComponent , Iterate};
