b25fe63df53e69ec9e901a8b600d5a626753c2d5
Marcus <ukendio@gmail.com>
2022-06-25 17:19:44 +0200

    fix merge to only accept two maps

--- lib/immutable.d.ts
+++ lib/immutable.d.ts
@@ -10,7 +10,7 @@ export const None: None
 
 export type NoneIfy<T> = Exclude<T, None>
 
-export function merge<T extends Array<object>>(...dictionaries: T): NoneIfy<UnionToIntersection<T[keyof T]>>
+export function merge<T extends [object, object]>(...dictionaries: T): NoneIfy<UnionToIntersection<T[keyof T]>>
 
 export function toSet<Lt>(list: Lt[]): Set<Lt>;
 
