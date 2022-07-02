ff9a4c0e4a176e4816154f45dc7d58d6c6804def
Marcus <ukendio@gmail.com>
2022-07-02 23:13:41 +0200

    add title to checkbox and button

--- lib/debugger/hookWidgets.d.ts
+++ lib/debugger/hookWidgets.d.ts
@@ -2,11 +2,11 @@ import { PatchOverride } from "@rbxts/plasma/out/Style"
 import { Widgets } from "@rbxts/plasma/out/widgets"
 
 export type HookWidgets = PatchOverride<Widgets, {
-        checkbox: () => {
+        checkbox: (title: string) => {
             clicked(): boolean
             checked(): boolean
         },
-        button: () => {
+        button: (title: string) => {
             clicked(): boolean
         },
         slider: (n: number) => number
