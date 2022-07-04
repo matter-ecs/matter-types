a9db00ae17d685dac2829e9a9df58cd39d392560
Ukendio <ukendio@gmail.com>
2022-07-04 03:05:08 +0100

    fix eol

--- lib/topoRuntime.d.ts
+++ lib/topoRuntime.d.ts
@@ -1,33 +1,26 @@
-import { System } from "./Loop"
-import { World } from "./World"
+import { System } from "./Loop";
+import { World } from "./World";
 
 type MatterFrameState = {
-    generation: boolean,
-    deltaTime: number,
-    dirtyWorlds: Array<World>
-}
+	generation: boolean;
+	deltaTime: number;
+	dirtyWorlds: Array<World>;
+};
 
-type Node<T extends Array<unknown>>= {
-    frame?: MatterFrameState,
-    currentSystem?: System<T>
-    system?: System<T>
-}
+type Node<T extends Array<unknown>> = {
+	frame?: MatterFrameState;
+	currentSystem?: System<T>;
+	system?: System<T>;
+};
 
-declare function start<T extends Array<unknown>>(node: Node<T>, fn: () => void): void
+declare function start<T extends Array<unknown>>(node: Node<T>, fn: () => void): void;
 
-declare function useHookState<T>(discriminator?: unknown, cleanupCallback?: (storage: T ) => boolean | void): T
+declare function useHookState<T>(discriminator?: unknown, cleanupCallback?: (storage: T) => boolean | void): T;
 
-declare function useFrameState(): MatterFrameState
+declare function useFrameState(): MatterFrameState;
 
-declare function useCurrentSystem<T extends Array<unknown>>(): System<T>
+declare function useCurrentSystem<T extends Array<unknown>>(): System<T>;
 
-declare function withinTopoContext(): boolean
+declare function withinTopoContext(): boolean;
 
-
-export {
-    start,
-    useHookState,
-    useFrameState,
-    useCurrentSystem,
-    withinTopoContext
-}
+export { start, useHookState, useFrameState, useCurrentSystem, withinTopoContext };
