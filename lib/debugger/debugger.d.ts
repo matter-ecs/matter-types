3dfffbc0a3add98f7e48c9a735c01bd43ca90e61
Jack T <jack@jackt.space>
2024-04-23 14:00:32 -0700

    Add loop parameter names

--- lib/debugger/debugger.d.ts
+++ lib/debugger/debugger.d.ts
@@ -15,50 +15,52 @@ import hoverInspect from "./widgets/hoverInspect";
 import { AnyEntity } from "../World";
 
 declare namespace Debugger {
-	interface CustomWidgets {
-		panel: typeof panel;
-		selectionList: typeof selectionList;
-		container: typeof container;
-		frame: typeof frame;
-		link: typeof link;
-		realmSwitch: typeof realmSwitch;
-		valueInspect: typeof valueInspect;
-		worldInspect: typeof worldInspect;
-		entityInspect: typeof entityInspect;
-		tooltip: typeof tooltip;
-		hoverInspect: typeof hoverInspect;
-	}
+  interface CustomWidgets {
+    panel: typeof panel;
+    selectionList: typeof selectionList;
+    container: typeof container;
+    frame: typeof frame;
+    link: typeof link;
+    realmSwitch: typeof realmSwitch;
+    valueInspect: typeof valueInspect;
+    worldInspect: typeof worldInspect;
+    entityInspect: typeof entityInspect;
+    tooltip: typeof tooltip;
+    hoverInspect: typeof hoverInspect;
+  }
 }
 
 declare class Debugger<T extends Array<unknown>> {
-	public enabled: boolean;
-	public authorize?: (player: Player) => boolean;
-	public componentRefreshFrequency?: number;
-	public findInstanceFromEntity?: (id: AnyEntity) => Instance | undefined;
+  public enabled: boolean;
+  public authorize?: (player: Player) => boolean;
+  public componentRefreshFrequency?: number;
+  public findInstanceFromEntity?: (id: AnyEntity) => Instance | undefined;
 
-	public constructor(plasma: Plasma);
+  public constructor(plasma: Plasma);
 
-	public show(): void;
+  public show(): void;
 
-	public hide(): void;
+  public hide(): void;
 
-	public toggle(): void;
+  public toggle(): void;
 
-	public connectPlayer(): void;
+  public connectPlayer(): void;
 
-	public disconnectPlayer(): void;
+  public disconnectPlayer(): void;
 
-	public autoInitialize(loop: Loop<T>): void;
+  public autoInitialize(loop: Loop<T>): void;
 
-	public replaceSystem(oldSystem: System<T>, newSystem: System<T>): void;
+  public replaceSystem(oldSystem: System<T>, newSystem: System<T>): void;
 
-	public switchToServerView(): void;
+  public switchToServerView(): void;
 
-	public switchToClientView(): void;
+  public switchToClientView(): void;
 
-	public draw<T extends Array<unknown>>(loop: Loop<T>): void;
+  public draw<T extends Array<unknown>>(loop: Loop<T>): void;
 
-	public getWidgets(): Widgets;
+  public getWidgets(): Widgets;
+
+  public loopParameterNames: Array<string>;
 }
 
 export = Debugger;
